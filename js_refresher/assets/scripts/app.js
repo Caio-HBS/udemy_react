d25c4d017c031235ef8aea5264dde4a355f6b2e6
Caio Bianchi <caiohbs23@gmail.com>
2023-10-18 12:04:19 -0300

    added exercices

---
+++ js_refresher/assets/scripts/app.js
@@ -0,0 +1,70 @@
+// // // import { apiKey } from "./utils.js"
+
+// // // import apiKey from "./utils.js"
+// // import { apiKey2, abc as content } from "./utils.js"
+// // // import * as util from "./utils.js"
+
+// // // console.log(util.apiKey2);
+// // console.log(content);
+
+// // const userMessage = "Hello World!";
+
+// // console.log(userMessage);
+// // console.log(userMessage);
+
+// // function createGreeting(username, message = "Hello!") {
+// //     // console.log(username);
+// //     // console.log(message);
+// //     return "Hi, my name is " + username + ". " + message;
+// // }
+
+// // const greeting1 = createGreeting("Caio");
+// // console.log(greeting1);
+
+// // const greeting2 = createGreeting("João", "How are you!?");
+// // console.log(greeting2);
+
+// // export default (username, message) => {
+// //     console.log("Hello!");
+// //     return username + ", " + message;
+// // } 
+
+// const user = {
+//     name: "Caio",
+//     age: 34,
+//     greet() {
+//         console.log("Hello!");
+//         console.log(this.age)
+//     }
+// };
+
+// console.log(user.name);
+// (user.greet());
+
+// class User {
+
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     greet() {
+//         console.log("Hey!");
+//     }
+// }
+
+// const user1 = new User("Caio", 24);
+// console.log(user1)
+
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies[0])
+
+// hobbies.push("Running");
+// console.log(hobbies);
+
+// const index = hobbies.findIndex(item => item === "Sports");
+// console.log(index);
+
+// const editedHobbies = hobbies.map(item => ({text: item}));
+// console.log(editedHobbies);
+
---
+++ js_refresher/assets/scripts/utils.js
@@ -0,0 +1,5 @@
+// export let apiKey = "hdaskjl323";
+
+export default "hdaskjl323";
+export let apiKey2 = "fasfda3213";
+export let abc = "abc";

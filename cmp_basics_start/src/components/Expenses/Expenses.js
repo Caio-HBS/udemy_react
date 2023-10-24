6fe9792071087263d626288b5fe26b6d01ad25ec
Caio Bianchi <caiohbs23@gmail.com>
2023-10-24 16:41:18 -0300

    added filter functionality

--- cmp_basics_start/src/components/Expenses/Expenses.js
+++ cmp_basics_start/src/components/Expenses/Expenses.js
@@ -13,6 +13,23 @@ export default function Expenses(props) {
     setSelectedYear(year);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +37,7 @@ export default function Expenses(props) {
           selected={selectedYear}
           onSelectedYear={selectedYearHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );

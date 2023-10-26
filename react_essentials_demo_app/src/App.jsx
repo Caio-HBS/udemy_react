58cbe5aea17d63f382e107e5a4fd2016ff9df061
Caio Bianchi <caiohbs23@gmail.com>
2023-10-26 17:20:27 -0300

    final version of essentials project

--- react_essentials_demo_app/src/App.jsx
+++ react_essentials_demo_app/src/App.jsx
@@ -1,71 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept";
-import TabButton from "./components/TabButton";
-
-import { CORE_CONCEPTS } from "./data.js";
-import { EXAMPLES } from "./data.js";
+import Examples from "./components/Examples";
+import CoreConcepts from "./components/CoreConcepts";
 
 export default function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function selectHandler(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} concept={conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => selectHandler("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => selectHandler("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => selectHandler("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => selectHandler("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {!selectedTopic && <p>Please select a Component.</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }

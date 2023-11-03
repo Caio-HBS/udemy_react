40da7b85b85f6db575887b3bfc4736d389e96493
Caio Bianchi <caiohbs23@gmail.com>
2023-11-03 11:18:08 -0300

    first timer iteration

--- timer_game_challenge/src/App.jsx
+++ timer_game_challenge/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 export default function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Less Easy" targetTime={5} />
+        <TimerChallenge title="Medium" targetTime={10} />
+        <TimerChallenge title="Nightmare" targetTime={15} />
+      </div>
     </>
   );
 }

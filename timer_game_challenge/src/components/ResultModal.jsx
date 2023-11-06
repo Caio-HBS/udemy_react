10c18f0bbfd4a12aa75bf7da38d13b8d20bbb37e
Caio Bianchi <caiohbs23@gmail.com>
2023-11-06 10:37:44 -0300

    Fixed modals and finished timer

--- timer_game_challenge/src/components/ResultModal.jsx
+++ timer_game_challenge/src/components/ResultModal.jsx
@@ -1,7 +1,13 @@
 import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
   const dialogElement = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -13,14 +19,15 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
 
   return (
     <dialog ref={dialogElement} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost ? <h2>You lost</h2> : <h2>You won!</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stop the timer with <strong>X seconds left.</strong>
+        You stop the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>

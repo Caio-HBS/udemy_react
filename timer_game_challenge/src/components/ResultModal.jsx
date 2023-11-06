ad73dee414ecc3e3e84077649d516daf882bb2e2
Caio Bianchi <caiohbs23@gmail.com>
2023-11-06 11:07:44 -0300

    final commit for project

--- timer_game_challenge/src/components/ResultModal.jsx
+++ timer_game_challenge/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainingTime, onReset },
@@ -8,6 +9,7 @@ const ResultModal = forwardRef(function ResultModal(
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -17,9 +19,9 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
-    <dialog ref={dialogElement} className="result-modal">
-      {userLost ? <h2>You lost</h2> : <h2>You won!</h2>}
+  return createPortal(
+    <dialog ref={dialogElement} className="result-modal" onClose={onReset}>
+      {userLost ? <h2>You lost</h2> : <h2>Your score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
@@ -30,7 +32,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 

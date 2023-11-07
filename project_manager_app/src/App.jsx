53e695b82d3394d25b9a32ebbc30a9cac506c959
Caio Bianchi <caiohbs23@gmail.com>
2023-11-07 10:36:48 -0300

    new components for creating projects

--- project_manager_app/src/App.jsx
+++ project_manager_app/src/App.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Sidebar from "./components/Sidebar.jsx";
 import AddNewProject from "./components/AddNewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 export default function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <AddNewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <AddNewProject />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }

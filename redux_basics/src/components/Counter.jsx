1394e08dc213c2a1572134ea072636767cd01362
Caio Bianchi <caiohbs23@gmail.com>
2023-11-24 11:16:17 -0300

    added new dependencies and made counter work

--- redux_basics/src/components/Counter.jsx
+++ redux_basics/src/components/Counter.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 export default function Counter() {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  function incrementHandler() {
+    dispatch({ type: "increment" });
+  };
+
+  function decrementHandler() {
+    dispatch({ type: "decrement" });
+  };
+
+  function toggleCounterHandler() {}
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );

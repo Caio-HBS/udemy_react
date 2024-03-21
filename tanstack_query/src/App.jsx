1db6a92682004772f584dfe6bcd1b480eb2be025
Caio Bianchi <caiohbs23@gmail.com>
2024-03-21 16:08:14 -0300

    implemented first tanstack features

--- tanstack_query/src/App.jsx
+++ tanstack_query/src/App.jsx
@@ -1,8 +1,10 @@
+import React from "react";
 import {
   Navigate,
   RouterProvider,
   createBrowserRouter,
 } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
@@ -37,8 +39,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

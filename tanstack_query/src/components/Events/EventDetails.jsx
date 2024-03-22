e1741e9b8f4ec7ec173a8e7e1b440387da10049d
Caio Bianchi <caiohbs23@gmail.com>
2024-03-22 11:09:45 -0300

    added single event view and functionality to it

--- tanstack_query/src/components/Events/EventDetails.jsx
+++ tanstack_query/src/components/Events/EventDetails.jsx
@@ -1,8 +1,41 @@
-import { Link, Outlet } from "react-router-dom";
+import React from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
+
+import { queryClient, fetchEvent, deleteEvent } from "../../util/http.js";
 
 import Header from "../Header.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { details: id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const {
+    mutate,
+    isPending: isMutationPending,
+    isError: isMutationError,
+    error: mutationError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete(event) {
+    event.preventDefault();
+    
+    mutate({ id });
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +44,42 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <p>Fetching event details...</p>}
+      {isError && (
+        <ErrorBlock
+          title="Failed to fetch event details."
+          message={
+            error.info?.message ||
+            "There was a problem trying to fetch the event details. Please try again later."
+          }
+        />
+      )}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={handleDelete}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img
+              src={`http://localhost:3000/${data.image}`}
+              alt="Event image."
+            />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date} @ {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
     </>
   );
 }

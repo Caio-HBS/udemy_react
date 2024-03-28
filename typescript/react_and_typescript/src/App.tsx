0d42e43febd3fa827d959825b140917dfd092dc9
Caio Bianchi <caiohbs23@gmail.com>
2024-03-28 11:15:20 -0300

    added create new todo functionality

--- typescript/react_and_typescript/src/App.tsx
+++ typescript/react_and_typescript/src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 import Todos from "./components/Todos.tsx";
+import NewTodo from "./components/NewTodo.tsx";
 
 import Todo from "./models/todo.ts";
 
 export default function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function handleCreateTodo(enteredText: string) {
+    const newTodo = new Todo(enteredText);
+
+    setTodos((prevState) => {
+      return prevState.concat(newTodo);
+    });
+  }
 
   return (
     <div>
+      <NewTodo onCreateTodo={handleCreateTodo} />
       <Todos items={todos} />
     </div>
   );
---
+++ typescript/react_and_typescript/src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{ onCreateTodo: (enteredText: string) => void }> = (
+  props
+) => {
+  const todoTextInput = useRef<HTMLInputElement>(null);
+
+  function submitHandler(event: React.FormEvent) {
+    event.preventDefault();
+
+    const enteredText = todoTextInput.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onCreateTodo(enteredText);
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text-input">Todo text</label>
+      <input type="text" id="text-input" ref={todoTextInput} />
+      <button>Create!</button>
+    </form>
+  );
+};
+
+export default NewTodo;

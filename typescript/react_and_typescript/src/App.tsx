b3518669831a3c74b0b002e8b40860d157e0e41d
Caio Bianchi <caiohbs23@gmail.com>
2024-03-28 10:14:31 -0300

    small code corrections

--- typescript/react_and_typescript/src/App.tsx
+++ typescript/react_and_typescript/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
 import "./App.css";
 
-function App() {
-  return <div>This is your project!</div>;
+export default function App() {
+  return (
+    <div>
+      This is your project!
+    </div>
+  );
 }
-
-export default App;

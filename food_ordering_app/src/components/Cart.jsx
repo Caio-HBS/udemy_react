b471b1263e77261fa9348aa051a82832d02dde67
Caio Bianchi <caiohbs23@gmail.com>
2024-04-02 11:17:08 -0300

    changes in modal and in cart items

--- food_ordering_app/src/components/Cart.jsx
+++ food_ordering_app/src/components/Cart.jsx
@@ -4,8 +4,9 @@ import CartContext from "../store/CartContext.jsx";
 import UserProgressContext from "../store/UserProgressContext.jsx";
 import { currencyFormatter } from "../util/formatting.js";
 
-import Button from "./UI/Button.jsx";
 import Modal from "./UI/Modal.jsx";
+import Button from "./UI/Button.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -16,20 +17,29 @@ export default function Cart() {
     0
   );
 
+  function closeCartHandler() {
+    userProgressCtx.hideCart();
+  }
+
   return (
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            {...item}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+            onIncrease={() => cartCtx.addItem(item)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
-        <Button>Go to checkout</Button>
+        <Button textOnly onClick={closeCartHandler}>
+          Close
+        </Button>
+        <Button onClick={closeCartHandler}>Go to checkout</Button>
       </p>
     </Modal>
   );

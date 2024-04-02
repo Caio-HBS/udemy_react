d99449114a9a4974d5f554cb0d1ef1e30f5b81ce
Caio Bianchi <caiohbs23@gmail.com>
2024-04-02 10:19:01 -0300

    wrapped app in context provider

--- food_ordering_app/src/App.jsx
+++ food_ordering_app/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
+import { CartContextProvider } from "./store/CartContext";
+
 import Meals from "./components/Meals";
 import Header from "./components/Header";
 
 export default function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <main>
         <Meals />
       </main>
-    </>
+    </CartContextProvider>
   );
 }
